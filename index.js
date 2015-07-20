7699fbe6b4bee814bb41b340a3df3e5b49c8cc21
Anders Bälter <anders.balter@npolar.no>
2015-07-20 11:45:31 +0200

    Added support module tasks

--- index.js
+++ index.js
@@ -4,11 +4,27 @@ var fs = require('fs');
 var onlyScripts = require('./util/scriptFilter');
 var path = require('path');
 var tasks = fs.readdirSync(path.resolve(__dirname, './tasks/')).filter(onlyScripts);
+var config = require('./config');
 
 var loadTasks = function(gulpInstance) {
   tasks.forEach(function(task) {
     require('./tasks/' + task)(gulpInstance);
   });
-}
+};
 
-module.exports = {loadTasks: loadTasks};
+var loadAppTasks = function(gulpInstance) {
+  loadTasks(gulpInstance);
+  gulpInstance.task('default', ['dev']);
+};
+
+var loadModuleTasks = function(gulpInstance) {
+  loadTasks(gulpInstance);
+  gulpInstance.task('default', ['lint', 'test']);
+  gulpInstance.watch([].concat(config.src.js, config.tests), ['lint', 'test']);
+};
+
+module.exports = {
+  loadTasks: loadAppTasks, // @deprecated
+  loadAppTasks: loadAppTasks,
+  loadModuleTasks: loadModuleTasks
+};
--- tasks/development.js
+++ tasks/development.js
@@ -2,14 +2,11 @@ var task = function(gulp) {
   'use strict';
 
   var runSequence = require('run-sequence').use(gulp);
-  var debug = require('gulp-debug');
 
   gulp.task('dev', function(cb) {
     global.isProd = false;
     runSequence('clean', 'lint', 'test', ['browserify', 'copy-all'], 'watch', cb);
   });
-
-  gulp.task('default', ['dev']);
 };
 
 module.exports = task;
