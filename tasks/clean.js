159ab8e4ae383d4e3da75590beb1bdfd3bacb7be
Anders Bälter <anders.balter@npolar.no>
2015-09-11 13:41:25 +0200

    Dependency bump

--- tasks/clean.js
+++ tasks/clean.js
@@ -4,7 +4,7 @@ var task = function(gulp, config) {
   var del    = require('del');
 
   gulp.task('clean', function(cb) {
-    del([config.dist.root], { force: true }, cb);
+    del([config.dist.root], { force: true }).then(function () { cb(); });
   });
 };
 
