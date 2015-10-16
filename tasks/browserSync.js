16ca0b7e1d8755d9d50b19ac2fcc3e22f17ba855
cnrdh <cnrdh@users.noreply.github.com>
2015-10-16 13:28:00 +0200

    Updated check for files vs queries to actually se if file or folder exists.

--- tasks/browserSync.js
+++ tasks/browserSync.js
@@ -1,8 +1,10 @@
 var task = function(gulp, config) {
   'use strict';
 
+  var path = require('path');
+  var fs = require('fs');
   var browserSync = require('browser-sync').create();
-  var html5Regex = new RegExp('\/'+config.name+'\/([^\.]+)$');
+  var html5Regex = new RegExp('\/'+config.name+'\/(.*)$');
 
   gulp.task('browserSync', function() {
 
@@ -11,16 +13,21 @@ var task = function(gulp, config) {
         // Serve both project root and dist to enable sourcemaps
         baseDir: [process.cwd(), config.dist.root],
         middleware: function (req, res, next) {
-          var path;
+          var location;
           // Enable CORS
           res.setHeader('Access-Control-Allow-Origin', '*');
           // Rewrite html5 urls
           var matches = html5Regex.exec(req.url);
-          if (req.method === 'GET' && matches) {
-            path = '/'+config.name+'/#!'+matches[1];
-            res.writeHead(302, {'Location': path});
+          //console.log('req', req.url);
+          var file = path.join(config.dist.root, req.url);
+          //console.log('file', file);
+          if (req.method === 'GET' && matches && !fs.existsSync(file)) {
+            //console.log('no file -> hashbang!');
+            location = '/'+config.name+'/#!'+matches[1];
+            res.writeHead(302, {'Location': location});
             res.end();
           } else {
+            //console.log('serve file');
             next();
           }
         }
